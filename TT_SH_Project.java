0186da3d38081d8768bb04122dc40b4edc9b238f
Thu Tran <tntran@smith.edu>
2021-05-09 22:33:23 +0700

    update main

--- TT_SH_Project.java
+++ TT_SH_Project.java
@@ -7,11 +7,12 @@ class TT_SH_Project {
     static ArrayList<Todo> completed = new ArrayList<>(); // arraylist that stores completed tasks
     static ArrayList<Todo> uncompleted = new ArrayList<>(); // arraylist that stores uncompleted tasks
     static Scanner scan = new Scanner(System.in);
-    static Scanner fin = null;
-    static File file = new File("tasks.txt");
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_RESET = "\u001B[0m";
     public static void main(String[] args) {
+        Scanner fin = null;
         try { // open fin 
-            fin = new Scanner(file); // read the file
+            fin = new Scanner(new File("tasks.txt")); // read the file
         } catch (IOException ex) { // IOException: input-output exception
             System.out.print(ex);
         }
@@ -33,9 +34,10 @@ class TT_SH_Project {
         }
         fin.close(); // close the file
         System.out.print("Today's date is: "); getDate(); // print current date
+        System.out.println(ANSI_GREEN + "\nDAILY TIME MANAGER" + ANSI_RESET + "\nThis is a tool for you to manage your time effectively by recording your daily tasks.");
         char op;
         do {
-            System.out.print("\nMENU\n1. View entire file\n2. View uncompleted tasks\n3. Add a new task\n4. Delete a task\n5. Time a task\n6. View completed tasks\n7. View total time taken on all the tasks\n0. Quit\nChoose an option (0 - 7): ");
+            System.out.print("\nMENU\n1. View all your tasks\n2. View uncompleted tasks\n3. Add a new task\n4. Delete a task\n5. Time a task\n6. View completed tasks\n7. View total time taken on all the tasks\n0. Quit\nChoose an option (0 - 7): ");
             op = scan.next().charAt(0);
             System.out.println();
             switch (op) {
@@ -43,22 +45,22 @@ class TT_SH_Project {
                     viewFile();
                     break;
                 case '2':
-                    printUncompleted(uncompleted);
+                    printUncompleted();
                     break;
                 case '3':
-                    enterTask(uncompleted);
+                    enterTask();
                     break;
                 case '4':
-                    removeTask(uncompleted, completed);
+                    removeTask();
                     break;
                 case '5':
-                    timeUncompleted(uncompleted);
+                    timeUncompleted();
                     break;
                 case '6':
-                    printCompleted(uncompleted, completed);
+                    printCompleted();
                     break;
                 case '7':
-                    totalTime(completed);
+                    totalTime();
                     break;
                 case '0':
                     saveFile();
@@ -69,21 +71,17 @@ class TT_SH_Project {
         } while (op != '0');
     } // main
 
-    static void viewFile() { // view all tasks in the file (in csv format)
-        try { // open fin 
-            fin = new Scanner(file); // read the file
-        } catch (IOException ex) { // IOException: input-output exception
-            System.out.print(ex);
+    static void viewFile() { // view all tasks in the file (formatting nicely)
+        ArrayList <Todo> fileList = new ArrayList <Todo> (); // create a fileList arraylist the stores all the tasks, uncompleted or completed
+        fileList.addAll(uncompleted);
+        fileList.addAll(completed);
+        for (int i = 0; i < fileList.size(); i++) { // print out all tasks
+            System.out.println((i + 1) + ". " + fileList.get(i) + " " + (fileList.get(i).getComplete() ? "completed" : "uncompleted"));
         }
-        while(fin.hasNext()){ // while next line still exists (read till end of file)
-            String task = fin.nextLine(); 
-            System.out.println(task); // print the task
-        }
-        fin.close(); // close the file
     } // viewFile
 
 
-    static void printUncompleted(ArrayList<Todo> uncompleted) { // show the list of uncompleted tasks
+    static void printUncompleted() { // show the list of uncompleted tasks
         for (int i = 0; i < uncompleted.size(); i++) {
             System.out.println((i + 1) + ". " + uncompleted.get(i));
         }
@@ -98,50 +96,54 @@ class TT_SH_Project {
             } while (toAdd != 'y' && toAdd != 'n');
                     
             if (toAdd == 'y') {
-                enterTask(uncompleted);
+                enterTask();
                 return;
             }   
         }
     } // printUncompleted
 
-    static void enterTask(ArrayList<Todo> uncompleted) { // add a new task
-        System.out.print("Enter a task: ");
+    static void enterTask() { // add a new task
+        ArrayList<String> allTasks = new ArrayList<>(); // arraylist to store all the task names
+        for (Todo u : uncompleted)
+            allTasks.add(u.getTask()); // add all uncompleted task names
+        for (Todo c : completed)
+            allTasks.add(c.getTask()); // add all completed task names
+        System.out.print("Enter a task: "); // ask for input
         scan.nextLine();
-        String task = scan.nextLine();
+        String task = scan.nextLine().toLowerCase();
+
+        while (allTasks.contains(task)) { // if arraylist contains task, ask again
+            System.out.println("Task already exists. Please add another task.");
+            System.out.print("Enter a task: ");
+            task = scan.nextLine().toLowerCase();
+        }
         Todo newTask = new Todo(task, 0, 0, 0, false); // create a new Todo object (task), uncompleted by default
-        uncompleted.add(newTask);
+        uncompleted.add(newTask); // add new task to the uncompleted list
         System.out.println("Task added.");
+        // System.out.println(uncompleted); // test
     } // enterTask
 
-    static void removeTask(ArrayList<Todo> uncompleted, ArrayList <Todo> completed) { // remove an existing task
+    static void removeTask() { // remove an existing task
         ArrayList <Todo> fileList = new ArrayList <Todo> (); // create a fileList arraylist the stores all the tasks, uncompleted or completed
         fileList.addAll(uncompleted);
         fileList.addAll(completed);
         for (int i = 0; i < fileList.size(); i++) { // print out all tasks
             System.out.println((i + 1) + ". " + fileList.get(i) + " " + (fileList.get(i).getComplete() ? "completed" : "uncompleted"));
         }
+
+        String removeString;
         int removeNum = 0;
-        do {
-            try {
-                System.out.print("Enter a task number to delete: "); // ask for the index (on the list) of the task to delete, which is shown on screen
-                removeNum = scan.nextInt();
-            } catch (IndexOutOfBoundsException ex) { // technically i don't need this?
-                System.out.println("Index out of bounds. Please try again.");
-                scan.nextLine();
-                return;
-                // System.out.print("Enter a task number to delete: "); 
-                // scan.nextLine();
-                // removeNum = scan.nextInt();
-            } catch (InputMismatchException ex) {
+        do { // check if it can be parsed as a number
+            System.out.print("Enter a task number to delete: ");
+            removeString = scan.next();
+            if (!checkInt(removeString)) // if it's an integer
                 System.out.println("Input must be a positive integer. Please try again.");
-                scan.nextLine();
-                return;
-                // System.out.print("Enter a task number to delete: "); 
-                // scan.nextLine();
-                // removeNum = scan.nextInt();  
-            }
-        } while (removeNum > fileList.size() || removeNum == 0);
-        
+            else {
+                removeNum = Integer.parseInt(removeString); // parse string as an integer
+                if (removeNum > fileList.size() || removeNum <= 0) // removeNum out of bounds, try again
+                    System.out.println("Index out of bounds. Please try again");
+            }   
+        } while (!checkInt(removeString) || removeNum > fileList.size() || removeNum <= 0); 
         
         for (int i = 0; i < uncompleted.size(); i++) { // remove the task from uncompleted arraylist if it's an uncompleted task
             if (fileList.get(removeNum-1).getTask().equals(uncompleted.get(i).getTask())) { // compare task to be deleted vs task in uncompleted arraylist
@@ -156,7 +158,7 @@ class TT_SH_Project {
         System.out.println("Task deleted.");
     } // removeTask
 
-    static void timeUncompleted(ArrayList<Todo> uncompleted) { // time a task that is marked as uncompleted
+    static void timeUncompleted() { // time a task that is marked as uncompleted
         for (int i = 0; i < uncompleted.size(); i++) {
             System.out.println((i + 1) + ". " + uncompleted.get(i));
         }
@@ -171,12 +173,25 @@ class TT_SH_Project {
             } while (toAdd != 'y' && toAdd != 'n');
 
             if (toAdd == 'y') {
-                enterTask(uncompleted);
+                enterTask();
                 return;
             }   
         }
-        System.out.print("Enter a task number to time that task: "); // ask for the index (on the list) of the task to time, which is shown on screen
-        int i = scan.nextInt(); // exception needed
+        
+        String indexString;
+        int i = 0;
+        do { // check if it can be parsed as a number
+            System.out.print("Enter a task number to time that task: "); // ask for the index (on the list) of the task to time, which is shown on screen
+            indexString = scan.next();
+            if (!checkInt(indexString)) // if it's an integer
+                System.out.println("Input must be a positive integer. Please try again.");
+            else {
+                i = Integer.parseInt(indexString); // parse string as an integer
+                if (i > uncompleted.size() || i <= 0) // removeNum out of bounds, try again
+                    System.out.println("Index out of bounds. Please try again");
+            }   
+        } while (!checkInt(indexString) || i > uncompleted.size() || i <= 0); 
+
         if (i > 0 && i <= uncompleted.size()) {
             Stopwatch stopwatch = new Stopwatch(); // create a stopwatch and run it
             Todo timedTask = uncompleted.get(i - 1); // task that is updated after timing
@@ -188,8 +203,15 @@ class TT_SH_Project {
                 uncompleted.set(i - 1, timedTask);
             }
             System.out.println("Task time modified: " + uncompleted.get(i - 1)); // inform the user about the updated task
-            System.out.print("Mark as complete (y/n)? "); // allow the user to decide whether they want to be done with the task, or come back to continue timing the task at another time during the day
-            char check = scan.next().charAt(0);
+            
+            char check;
+            do {
+                System.out.print("Mark as complete (y/n)? "); // allow the user to decide whether they want to be done with the task, or come back to continue timing the task at another time during the day
+                check = Character.toLowerCase(scan.next().charAt(0)); // case-insensitive
+                if (check != 'y' && check != 'n')
+                    System.out.println("Input must be y or n. Please try again.");
+            } while (check != 'y' && check != 'n');
+
             if (check == 'y') {
                 uncompleted.get(i - 1).setComplete(true); // change complete field to true (mark as completed)
                 completed.add(uncompleted.get(i - 1)); // move task to completed arraylist
@@ -199,7 +221,7 @@ class TT_SH_Project {
     } // timeUncompleted
 
 
-    static void printCompleted(ArrayList<Todo> uncompleted, ArrayList<Todo> completed) { // print all completed tasks
+    static void printCompleted() { // print all completed tasks
         for (int i = 0; i < completed.size(); i++) {
             System.out.println((i + 1) + ". " + completed.get(i));
         }
@@ -208,7 +230,7 @@ class TT_SH_Project {
     } // printCompleted
 
 
-    static void totalTime(ArrayList<Todo> completed) { // calculate and show the total time taken for all the completed tasks during the day
+    static void totalTime() { // calculate and show the total time taken for all the completed tasks during the day
         int total = 0; // in seconds
         for (int i = 0; i < completed.size(); i++) { // split the total (in seconds) to hours, minutes and seconds
             total += completed.get(i).getHours() * 3600; 
@@ -227,7 +249,7 @@ class TT_SH_Project {
     } // totalTime
 
     static void saveFile() { // allow the user to save their progress in a file after they choose to quit the program
-        int toSave;
+        char toSave;
         do {
             System.out.print("Do you want to save all the tasks into the file tasks.txt for today (y/n)? "); // ask them whether they want to save their progress or not
             toSave = Character.toLowerCase(scan.next().charAt(0)); // case-insensitive
@@ -240,7 +262,7 @@ class TT_SH_Project {
         else { // if yes, then update in a file and say goodbye
             PrintWriter fout = null;
             try {
-                fout = new PrintWriter(file); // write in a new file
+                fout = new PrintWriter("tasks.txt"); // write in a new file
             } catch (IOException ex) { // IOException: input-output exception
                 System.out.print(ex);
             }
@@ -254,6 +276,14 @@ class TT_SH_Project {
             fout.close();
         }
     } // saveFile
+
+    static boolean checkInt(String s) { // check if string is an int
+        for (int i = 0; i < s.length(); i++) {
+            if (!Character.isDigit(s.charAt(i)))
+                return false;
+        }
+        return true;
+    } // checkInt
 
     static void getDate() { // get current date
         DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("MM/dd/yyyy"); 
